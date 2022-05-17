fd0761a2e104e43198f072224e21305adb5754d5
Dv-Joan <joan300501@gmail.com>
2022-05-17 11:12:37 -0500

    modified

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import Card from './Components/Card';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='bg-gradient-to-r from-fuchsia-300 to-violet-700 flex justify-center'>
+      <div className='bg-gradient-to-r from-purple-500 to-blue-400 h-96 w-96 rounded-3xl  border-4 border-solid mx- my-16 px-10 py-5 border-blue-900 '>
+        <div className='flex-1'>
+          <div id='Logo' className='mb-7 flex'>
+            <img
+              src='https://iconape.com/wp-content/files/rf/369356/svg/dollar-1-logo-icon-png-svg.png'
+              className='mt-3 h-9 w-10 mr-2'
+            />
+            <div className='text-white   items-center'>
+              <span className='font-black text-2xl'>STT</span>
+              <br></br>
+              <span className='tracking-wide'>group</span>
+            </div>
+          </div>
+          <div>
+            <span className='text-yellow-400 text-4xl font-black tracking-normal'>
+              Improve
+            </span>
+            <br></br>
+            <span className='text-white text-4xl font-black tracking-normal '>
+              your bussiness
+            </span>
+            <span className='text-white text-normal tracking-normal '>
+              <br></br>
+              Increase Sales in the first week
+            </span>
+          </div>
+        </div>
+        <div className='pl-5 bg-blue-200'>
+          <img
+            src='https://support.apple.com/content/dam/edam/applecare/images/en_US/psp/featured-section-new-in-macos_2x.png'
+            className='w-2/3 h-2/3'
+          />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Components/Card.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Card() {
+  return (
+    <div>
+      <div>
+        <div className=''>
+          <img src='../Components/Images/1.png' alt='Image-1' />
+        </div>
+        <div>
+          <h2>Rapid Implementation</h2>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
