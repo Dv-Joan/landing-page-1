27cefbc6368592afb7f5d00216500242b2f09935
Dv-Joan <joan300501@gmail.com>
2022-05-18 10:48:15 -0500

    Modified 1.0.1 by Krammer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import Card from './Components/Card';
+import Image from './Components/Images/banner_image_2.webp';
+import Logo from './Components/Images/logo.png';
 
 function App() {
   return (
     <div className='bg-gradient-to-r from-fuchsia-300 to-violet-700 flex justify-center'>
-      <div className='bg-gradient-to-r from-purple-500 to-blue-400 h-96 w-96 rounded-3xl  border-4 border-solid mx- my-16 px-10 py-5 border-blue-900 '>
-        <div className='flex-1'>
-          <div id='Logo' className='mb-7 flex'>
-            <img
-              src='https://iconape.com/wp-content/files/rf/369356/svg/dollar-1-logo-icon-png-svg.png'
-              className='mt-3 h-9 w-10 mr-2'
-            />
+      <div className='bg-gradient-to-r from-purple-500 to-blue-400 w-3/4 rounded-3xl  border-4 border-solid mx- my-16 px-14 py-7 border-blue-900 '>
+        <div className='flex-grid columns-2'>
+          <div id='Logo' className='mb-7 flex pb-5'>
+            <img src={Logo} className='mt-2 h-12 w-12 mr-2' />
             <div className='text-white   items-center'>
               <span className='font-black text-2xl'>STT</span>
               <br></br>
@@ -17,24 +16,28 @@ function App() {
             </div>
           </div>
           <div>
-            <span className='text-yellow-400 text-4xl font-black tracking-normal'>
+            <span className='text-yellow-400 text-6xl font-black tracking-normal'>
               Improve
             </span>
             <br></br>
-            <span className='text-white text-4xl font-black tracking-normal '>
+            <span className='text-white text-6xl font-black tracking-normal '>
               your bussiness
             </span>
-            <span className='text-white text-normal tracking-normal '>
-              <br></br>
+            <div className='text-white text-xl tracking-normal pt-4 '>
               Increase Sales in the first week
-            </span>
+            </div>
+            <div className='pt-16'>
+              <button className='ease-out duration-300 rounded-full text-xl text-white tracking-wide font-black drop-shadow-md bg-gradient-to-r from-slate-700 to-purple-700 px-12 py-2'>
+                Improve
+              </button>
+            </div>
           </div>
-        </div>
-        <div className='pl-5 bg-blue-200'>
-          <img
-            src='https://support.apple.com/content/dam/edam/applecare/images/en_US/psp/featured-section-new-in-macos_2x.png'
-            className='w-2/3 h-2/3'
-          />
+          <div className=''>
+            {/* <div className='pl-5 bg-red-400 my-2 mx-2 '>
+            <Card />
+          </div> */}
+            <img src={Image} className='drop-shadow-xl' />
+          </div>
         </div>
       </div>
     </div>
